f20fc0f572fe327e5cc15143b69d41757b98d39d
Diegokernel <777@holbertonschool.com>
2019-03-16 16:22:40 -0500

    add header file

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct print
+{
+  char *p;
+  void (*f)(va_list ap1);
+} print_t;
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+#endif
