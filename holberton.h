447815f292b7aadd06b7585a5cbab224a9971335
Diegokernel <777@holbertonschool.com>
2019-03-18 21:50:07 +0000

    add new prototypes

--- holberton.h
+++ holberton.h
@@ -10,12 +10,14 @@
  */
 typedef struct print
 {
-	char *p;
-	void (*f)(va_list ap1);
+	char p;
+	int (*f)(va_list ap1);
 } prt_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
-int (*sp(const char *format))(va_list);
-int cha(va_list chara);
-int str(va_list strn);
+int (*check_mod(char com))(va_list);
+int print_c(va_list character);
+int print_s(va_list string);
+int int_i(va_list inte);
+int int_d(va_list inte);
 #endif
