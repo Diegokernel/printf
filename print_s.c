fd9ec0dd0763881b84741baf49db7571eecd3527
dianaboada <dcboadab@gmail.com>
2019-03-18 20:59:41 +0000

    Function: print string

---
+++ print_s.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/*
+* print_s - Function that prints strings in printf function.
+* @string: string to print.
+*
+*/
+int print_s(valist string)
+{
+	va_start string;
+	int i;
+	char *s;
+
+	s= (char)va_arg(string, char *);
+
+	if (s == "\0")
+	{
+		s = "NULL";
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		i = i + putchar(s[i]);
+	}
+	return (i);
+}
+
