e04f3a0daad830921eb67c8de6cff6535fbd8841
Diegokernel <777@holbertonschool.com>
2019-03-18 15:39:16 +0000

    modify the style of the header

--- holberton.h
+++ holberton.h
@@ -3,14 +3,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct print - struct to define pointers
+ * @p:  data type char
+ * @f: function asociated.
+ */
 typedef struct print
 {
-  char *p;
-  void (*f)(va_list ap1);
+	char *p;
+	void (*f)(va_list ap1);
 } prt_t;
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*sp(const char *format))(va_list);
 int cha(va_list chara);
-
+int str(va_list strn);
 #endif
