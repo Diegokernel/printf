2ebc664b8043f522242fa196d0b7b7ea28bb69e5
Diegokernel <777@holbertonschool.com>
2019-03-18 21:53:41 +0000

    add printf func

---
+++ printf.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+* check_mod - check for modifiers
+* @com: to compare with structure members
+* Return: count of char
+*/
+int _printf(const char *format, ...)
+{
+	int i, j;
+	va_list ap1;
+	int (*sp)(va_list);
+
+	j = 0;
+	va_start(ap1, format);
+	i = 0;
+	if (format == NULL)
+		return (-1);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			for (; format[i + 1] == ' '; i++)
+				;
+			if (format[i + 1] == '\0')
+				return (-1);
+			sp = check_mod(format[i + 1]);
+			if (sp != NULL)
+				j += sp(ap1);
+			else
+			{
+				j += _putchar('%');
+				if (format[i + 1] != '%')
+					j += _putchar(format[i + 1]);
+
+			}
+			i++;
+		}
+		else
+			j += _putchar(format [i]);
+		i++;
+	}
+	va_end(ap1);
+	return (j);
+}
