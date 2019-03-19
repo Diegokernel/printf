1d21b4ec29b7ab1defd64de89e40a99795776a61
dianaboada <dcboadab@gmail.com>
2019-03-19 01:09:11 +0000

    Betty style function int_i

--- int_d.c
+++ int_d.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
  * int_d - print an integer with d specifier
- * @inte: arguement
+ * @ap1: arguement
  * Return: integer.
  */
 int int_d(va_list ap1)
 {
 	unsigned int i;
 	int num, count, lon;
+
 	count = 0;
 	lon = 1;
 
